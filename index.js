aaaedcaaafb00cdfbc62b716a70744b1024cc093
Ludeo <lucas.steuer1@gmx.de>
2023-03-18 04:33:23 +0100

    switched to table design of leaderboard of player scores; loading database values on player profile init; moved links on player profile; changed manifest

--- index.js
+++ index.js
@@ -405,3 +405,75 @@ app.get(`${CONFIG.urlPrefix}/tags/:playerid`, async function(req, res) {
        }
    }
 });
+
+app.get(`${CONFIG.urlPrefix}/playerNoUpdate/:playerid`, async function(req, res) {
+   let conn;
+
+   try {
+       conn = await pool.getConnection();
+       let playerRaw = await conn.query("select * from `players` where id='" + req.params.playerid + "'");
+
+       if(playerRaw[0]['id'] === null) {
+           res.header("Access-Control-Allow-Origin", "*");
+           res.send(JSON.stringify(null));
+           return;
+       }
+
+       let player = playerRaw[0];
+       let country;
+
+       if(player.country === "none") {
+           country = "Unknown Country";
+       } else {
+           let regionNames = new Intl.DisplayNames(['en'], {type: 'region'});
+           country = regionNames.of(player.country.toUpperCase());
+       }
+
+       let scoresRaw = await conn.query("select * from `scores` where player_id='" + req.params.playerid + "'");
+       let scores = [];
+
+       for(let i = 0; i < scoresRaw.length; i++) {
+           let scoreRaw = scoresRaw[i];
+           let scoreInfo = scoresCsv.find(x => x.includes(',' + scoreRaw.map_id));
+
+           let score = {
+               id: scoreRaw.map_id,
+               grade: scoreRaw.grade,
+               mapname: scoreInfo.split(',')[0],
+               image: "",
+               maxpoints: scoreInfo.split(',')[1],
+               date: scoreRaw.date,
+               hardcore: scoreRaw.hardcore,
+               accuracy: scoreRaw.accuracy,
+               misses: scoreRaw.misses,
+               weightedpp: "",
+               pp: scoreRaw.pp
+           }
+
+           scores.push(score);
+       }
+
+       scores = scores.sort((a, b) => b.pp - a.pp);
+
+       let playerInfo = {
+           picture: player.image,
+           name: player.name,
+           globalRank: player.rank,
+           country: country,
+           countryShort: player.country,
+           countryRank: player.country_rank,
+           points: player.points,
+           weightedpp: player.pp,
+           scores: scores
+       };
+
+       res.header("Access-Control-Allow-Origin", "*");
+       res.send(JSON.stringify(playerInfo));
+   } catch(err) {
+       console.log(err);
+   } finally {
+       if(conn) {
+           await conn.end();
+       }
+   }
+});
