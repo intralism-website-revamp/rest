334f4e96b4716f8c7e35ec31f166eb7cdd80de91
Ludeo <lucas.steuer1@gmx.de>
2023-03-29 21:17:33 +0200

    added mods to mod page

--- index.js
+++ index.js
@@ -535,6 +535,52 @@ app.get(`${CONFIG.urlPrefix}/userInfo/:email`, validateAccessToken, async functi
     }
 });
 
+app.get(`${CONFIG.urlPrefix}/mods`, async function(req, res) {
+    let conn;
+    try {
+        conn = await pool.getConnection();
+        let mods = await conn.query("SELECT * FROM `mods`");
+
+        for(let i = 0; i < mods.length; i++) {
+            let mod = mods[i];
+            let dependencies = await conn.query("SELECT * FROM `mod_dependencies` where mod_id='" + mod.id + "'");
+            let authorName = await conn.query("SELECT * FROM `players` WHERE id='" + mod.author + "'");
+
+            if(authorName.length === 0) {
+                mod.authorName = "Unknown";
+            } else {
+                mod.authorName = authorName[0].name;
+            }
+
+            if(dependencies.length === 0) {
+                continue;
+            }
+
+            mod.dependencies = [];
+
+            for(let j = 0; j < dependencies.length; j++) {
+                let dependency = dependencies[j];
+                let dependencyModRaw = await conn.query("SELECT * FROM `mods` WHERE id='" + dependency.dependency_id + "'");
+                let dependencyMod = dependencyModRaw[0];
+
+                mod.dependencies.push({
+                    name: dependencyMod.name,
+                    version: dependencyMod.version,
+                    downloadLink: dependencyMod.download_link
+                });
+            }
+        }
+
+        res.send(JSON.stringify(mods));
+    } catch(err) {
+        console.log(err);
+    } finally {
+        if(conn) {
+            await conn.end();
+        }
+    }
+});
+
 async function updatePlayer(player) {
     let updateTime = new Date();
     updateTime.setFullYear(1970);
