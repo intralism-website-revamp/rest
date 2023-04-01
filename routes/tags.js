8501aa921e47f715fcb2c35abc9be79df0fa61ff
Ludeo <lucas.steuer1@gmx.de>
2023-04-01 19:42:34 +0200

    added first and last page to be always visible in tables navigation; fixed route of tags

--- routes/tags.js
+++ routes/tags.js
@@ -1,45 +1,25 @@
 const express = require('express');
 const router = express.Router();
-const playerFunctions = require("../functions/playerFunctions");
 const pool = require("../dbConnection");
 
 router
-    .get('/:id', async function(req, res){
-        let player = await playerFunctions.UpdatePlayerFromWebsite(req.params.id, true);
-
-        player.missingScores = [];
-
-        let maps, conn;
+    .get('/:playerid', async function(req, res){
+        let conn;
 
         try {
             conn = await pool.getConnection();
-            maps = await conn.query("select * from `maps`");
-        } catch (err) {
+            let tags = await conn.query("select t.name, t.order from `tags` as t, `player_tags` as pt where player_id='" + req.params.playerid + "' and t.id = pt.tag_id");
+
+            tags = tags.sort((a, b) => b.id - a.id);
+
+            res.send(JSON.stringify(tags));
+        } catch(err) {
             console.log(err);
         } finally {
             if(conn) {
                 await conn.end();
             }
         }
-
-        for(let j = 0; j < maps.length; j++) {
-            if(player.scores.find(x => BigInt(x.id) === BigInt(maps[j].id)) === undefined) {
-                let map = maps[j];
-
-                let missingScore = {
-                    id: map.id,
-                    name: map.name,
-                    pp: map.pp,
-                    points: map.points,
-                    image: map.image
-                };
-                player.missingScores.push(missingScore);
-            }
-        }
-
-        player.missingScores = player.missingScores.sort((a, b) => b.pp - a.pp);
-
-        res.send(JSON.stringify(player));
     });
 
 module.exports = router;
